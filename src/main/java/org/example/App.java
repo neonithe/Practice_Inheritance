a0aaa6b84e6fd134b4a0965096bb48dd88e4cec0
Mattias Andersson <neonithe@gmail.com>
2020-05-12 07:55:50 +0200

    More cleanup

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,28 +1,8 @@
 package org.example;
 
-import org.example.Employee.Employee;
 import org.example.Employee.SalesPerson;
 import org.example.Employee.SystemDeveloper;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-
-/**
- * When hired each employee has a base salary of 25 000 a month
- * •
- * SystemDevelopers get 1000 extra for each certificate they have and 1500 extra for each
- * programming language they work with
- * •
- * SalesPersons get 500 extra for each client they maintain and 1000 extra for each new client they
- * have aquired
- * •
- * Create method calculateSalary () in Super class and override this method in its subclasses
- * •
- * Also make a toString implementation for SystemDevelopers and SalesPerson
- * •
- * Whenever they aquire new clients , certificates and languages the salary should update
- *
- */
 public class App 
 {
 
